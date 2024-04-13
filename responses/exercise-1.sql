3942a9a42d92b6456d5364ccb3ca10d7849d1c43
CrimsonGee <nelsongee5@gmail.com>
2024-04-12 23:51:39 -0400

    Update exercise-1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -1,2 +1,12 @@
 -- write your SQL here
-select 1;
+SELECT
+	o.locationId,
+	EXTRACT(YEAR_MONTH FROM t.datetime) as Month_Year,
+	SUM(refund.amount) as Refund_Amount
+	from transactions t, 
+		 JSON_TABLE(details, '$.items[*]' COLUMNS(
+		id INT PATH '$.id',
+		amount INT PATH '$.amount')) refund
+		inner join orderItems o on o.id=refund.id
+		group by o.locationId, Month_Year
+		order by o.locationId;
